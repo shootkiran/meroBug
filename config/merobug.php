f5a67c1408ea870bb694c34556b813dbc9bbf0f9
Kiran Shrestha <shootkiran@gmail.com>
2022-10-28 22:54:06 +0545

    updated

---
+++ config/merobug.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+
+    'lines_count' => 12,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Prevent duplicates
+    |--------------------------------------------------------------------------
+    |
+    | Set the sleep time between duplicate exceptions. This value is in seconds, default: 60 seconds (1 minute)
+    |
+    */
+
+    'sleep' => 60,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Skip exceptions
+    |--------------------------------------------------------------------------
+    |
+    | List of exceptions to skip sending.
+    |
+    */
+
+    'except' => [
+        'Symfony\Component\HttpKernel\Exception\NotFoundHttpException',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Key filtering
+    |--------------------------------------------------------------------------
+    |
+    | Filter out these variables before sending them to LaraBug
+    |
+    */
+
+    'blacklist' => [
+        '*authorization*',
+        '*password*',
+        '*token*',
+        '*auth*',
+        '*verification*',
+        '*credit_card*',
+        'cardToken', // mollie card token
+        '*cvv*',
+        '*iban*',
+        '*name*',
+        '*email*'
+    ],
+
+];
